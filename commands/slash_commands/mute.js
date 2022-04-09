843ecce31be4715bcdcac0421873ea538c5def2f
loliknight <loliknight@easygaming.su>
2022-04-09 09:16:07 +0000

    some changes

--- commands/slash_commands/mute.js
+++ commands/slash_commands/mute.js
@@ -197,13 +197,7 @@ class Command extends Command_template {
         }
       }
     } catch (error) {
-      console.log(
-        `Произошла ошибка при исполнении команды ${this.interaction.commandName}`
-      );
-      let errors_channel = Bot.bot.channels.cache.get(f.config.errorsChannel);
-      errors_channel.send(
-        `Ошибка при исполнении команды \`${this.interaction.commandName}\`:\n\`${error.name}: ${error.message}\``
-      );
+      f.handle_error(error, `/- команда ${this.interaction.commandName}`);
     }
   }
 
